90d956de14a94b883cea3351e64f5172bb029fd3
pqkhanh <pqkhanh@tma.com.vn>
2021-05-07 10:58:16 +0700

    first code

---
+++ backend/dashboard/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6472aca4cf688ea4457f88398ac6e657'),
+    },
+  },
+});
---
+++ backend/dashboard/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1e512a01-ced2-406b-a7fa-883a72a263f3'
+};
